e8fb2e7e3487a6e63a0ecb846bbe47953858abf6
Kaikai Liu <kaikai.liu@sjsu.edu>
2022-07-17 22:02:37 -0700

    Waymo train and test in HPC 4 GPUs

--- testWaymo.py
+++ testWaymo.py
@@ -290,7 +290,7 @@ def test(data,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
     parser.add_argument('--weights', nargs='+', type=str, default='/home/010796032/MyRepo/modelzoo/waymoyolov7trained/waymoyolov7epoch99.pt', help='model.pt path(s)')
-    parser.add_argument('--data', type=str, default='data/mycocohpc.yaml', help='*.data path,mycocohpc.yaml, waymococohpc.yaml')
+    parser.add_argument('--data', type=str, default='data/waymococohpc.yaml', help='*.data path,mycocohpc.yaml, waymococohpc.yaml')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
